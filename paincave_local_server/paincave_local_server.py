7b9f388bb20cce34e9578e3d0b4d87c8701d871e
Simon Vikstrom <sv@one.com>
2015-03-21 14:23:17 +0100

    Poooo

--- paincave_local_server/paincave_local_server.py
+++ paincave_local_server/paincave_local_server.py
@@ -3,7 +3,6 @@ import time
 import struct
 import threading
 import json
-from pprint import pprint
 
 from websocket import WebSocketsServer
 
@@ -85,23 +84,6 @@ class ANT_SERVER():
         return self
 
   def _setup_channels(self):
-
-    #node = Node()
-    #node.set_network_key(0x00, NETWORK_KEY)
-
-    #channel = node.new_channel(Channel.Type.BIDIRECTIONAL_RECEIVE)
-
-    #channel.on_broadcast_data = monitor.on_data_heartrate
-    #channel.on_burst_data = monitor.on_data_heartrate
-
-    #channel.set_period(8070)
-    #channel.set_search_timeout(12)
-    #channel.set_rf_freq(57)
-    #channel.set_id(0, 120, 0)
-
-
-
-
     self.antnode = Node()
     self.antnode.set_network_key(0x00, self.netkey)
 
@@ -180,7 +162,6 @@ class ANT_SERVER():
 
 
 if __name__ == "__main__":
-  NETKEY = 'B9A521FBBD72C345'.decode('hex')
   NETKEY = [0xb9, 0xa5, 0x21, 0xfb, 0xbd, 0x72, 0xc3, 0x45]
 
   websocket_ant_server = WEBSOCKET_ANT_SERVER()
