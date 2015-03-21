14e78306f08951a92f0ec1138a08ad87579881d2
Simon Vikstrom <sv@one.com>
2015-03-21 22:20:33 +0100

    Wee, all the awesoe format

---
+++ json_format.js
@@ -0,0 +1,35 @@
+{
+    "intervals": [
+        {
+            "duration": "00:05:00",
+            "instructions": [
+                {
+                    "duration": "6s",
+                    "offset": "00:00:00",
+                    "text": "Pooop"
+                },
+                {
+                    "duration": "6s",
+                    "offset": "00:20:00",
+                    "text": "Pooop 2"
+                },
+                {
+                    "duration": "6s",
+                    "match": [
+                        {
+                            "cad": ">100"
+                        },
+                        {
+                            "cad": "<50"
+                        }
+                    ],
+                    "offset": "00:20:00",
+                    "text": "n00b"
+                }
+            ],
+            "target_power_end": "300",
+            "target_power_start": "200",
+            "type": "rest"
+        }
+    ]
+}
