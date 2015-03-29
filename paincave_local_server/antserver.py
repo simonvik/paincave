30599cd211ed0a83fa9cc8525beac9592afea0c6
Konrad Beckmann <konrad.beckmann@gmail.com>
2015-03-29 16:10:48 +0200

    Moved out AntServer

---
+++ paincave_local_server/antserver.py
@@ -0,0 +1,149 @@
+# global imports
+import json
+import os.path
+import time
+from datetime import datetime
+
+# local imports
+import antparsers
+
+try:
+  from ant.easy.node import Node
+  from ant.easy.channel import Channel
+  from ant.base.message import Message
+  from ant.easy.exception import AntException
+except ImportError as e:
+  print("Failed to load ant libs : \n%s" % e)
+  print("Download ant-lib from https://github.com/simonvik/openant")
+
+
+class AntServer():
+  def __init__(self, netkey, ant_devices, was):
+    self.ant_devices = ant_devices
+    self.channels = []
+    self.netkey = netkey
+    self.antnode = None
+    self.was = was
+    self.ant_modes = {
+      "speed_cad" : {
+        "device_id" : 121,
+        "period" : 8086,
+        "freq" : 57,
+        "ant_name" : "C:CAD",
+        "handler" : "_handle_speed_cad"
+      },
+      "hr" : {
+        "device_id" : 120,
+        "period" : 8070,
+        "freq" : 57,
+        "ant_name" : "C:HRM",
+        "handler" : "_handle_hr"
+      },
+      "power" : {
+        "device_id" : 11,
+        "period" : 8182,
+        "freq" : 57,
+        "ant_name" : "C:PWR",
+        "handler" : "_handle_power"
+      }
+    }
+
+    self.parsers = {
+      "hr" : antparsers.Hr(),
+      "power" : antparsers.Power(),
+      "speed_cad" : antparsers.SpeedCadence()
+    }
+    self._log_raw_data = False
+    self._log_decoded = True
+
+
+  @staticmethod
+  def setup_and_start(netkey, ant_devices, was, log_config):
+    antserver = None
+    for i in range(1, 3):
+      try:
+        antserver = AntServer(netkey, ant_devices, was)
+        antserver._log_raw_data = log_config["log_raw_data"]
+        antserver._log_decoded = log_config["log_decoded"]
+        try:
+          antserver.start()
+        except KeyboardInterrupt:
+          pass
+        finally:
+            antserver.stop()
+        break
+      except AntException:
+        print("ERR: Failed to setup ant server. Retrying... %s" % i)
+
+  def start(self):
+    if not os.path.isdir("logs"):
+      os.mkdir("logs")
+    self.logfile = datetime.strftime(datetime.now(), "logs/%Y%m%d_%H%M.txt")
+    self.logfile_handle = open(self.logfile, "w")
+
+    self._setup_channels()
+
+  def stop(self):
+    self.antnode.stop()
+
+  def __enter__(self):
+    return self
+
+  def _setup_channels(self):
+    self.antnode = Node()
+    self.antnode.set_network_key(0x00, self.netkey)
+
+    for device in self.ant_devices:
+      print("Registering device : %s : %s" % (device, self.ant_modes[device]))
+      dev = self.ant_modes[device]
+
+      c = self.antnode.new_channel(Channel.Type.BIDIRECTIONAL_RECEIVE)
+      c.set_id(0, dev["device_id"], 0)
+      c.set_search_timeout(12)
+      c.set_period(dev["period"])
+      c.set_rf_freq(dev["freq"])
+      m = getattr(self, dev["handler"])
+      c.on_broadcast_data = m
+      c.on_burst_data = m
+      c.open()
+      self.channels.append(c)
+
+    self.antnode.start()
+
+  def _log_raw(self, event_type, msg):
+    if self._log_raw_data:
+      data = []
+      for m in msg:
+        data.append(m)
+      m = json.dumps({"raw" : "true",
+                      "event_type" : event_type,
+                      "time_millis" : int(time.time() * 1000),
+                      "data" : data})
+      self.logfile_handle.write(m)
+      self.logfile_handle.write("\n")
+      self.logfile_handle.flush()
+      os.fsync(self.logfile_handle.fileno())
+
+  def _parse_and_send(self, event_type, data):
+    self._log_raw(event_type, data)
+
+    parser = self.parsers[event_type]
+    values = parser.parse(data)
+    if values:
+      for value in antparsers.Parser.to_json(values):
+        self.was.send_to_all(value)
+        if self._log_decoded:
+          print(value)
+
+  def _handle_hr(self, data):
+    self._parse_and_send("hr", data)
+
+  def _handle_speed_cad(self, data):
+    self._parse_and_send("speed_cad", data)
+
+  def _handle_power(self, data):
+    self._parse_and_send("power", data)
+
+  def __exit__(self, type_, value, traceback):
+    self.stop()
+    self.logfile_handle.close()
--- paincave_local_server/paincave_local_server.py
+++ paincave_local_server/paincave_local_server.py
@@ -1,26 +1,19 @@
-import sys
-import time
-import struct
-import threading
-import json
-import random
-import time
-import os.path
-
-import antparsers
-
-from datetime import datetime
-from websocket import WebSocketsServer
+# global imports
 import argparse
+import json
+import os.path
+import random
+import struct
+import sys
+import threading
+import time
+from datetime import datetime
 
-try:
-  from ant.easy.node import Node
-  from ant.easy.channel import Channel
-  from ant.base.message import Message
-  from ant.easy.exception import AntException
-except ImportError as e:
-  print("Failed to load ant libs : \n%s" % e)
-  print("Download ant-lib from https://github.com/simonvik/openant")
+# local imports
+import antparsers
+from antserver import AntServer
+from websocket import WebSocketsServer
+
 
 class WebsocketAntServer:
   def __init__(self):
@@ -51,119 +44,6 @@ class WebsocketAntServer:
     print("Stopping websocket server")
     self.server.shutdown()
     self.server.killall()
-
-
-class AntServer():
-  def __init__(self, netkey, ant_devices, was):
-    self.ant_devices = ant_devices
-    self.channels = []
-    self.netkey = netkey
-    self.antnode = None
-    self.was = was
-    self.ant_modes = {
-      "speed_cad" : {
-        "device_id" : 121,
-        "period" : 8086,
-        "freq" : 57,
-        "ant_name" : "C:CAD",
-        "handler" : "_handle_speed_cad"
-      },
-      "hr" : {
-        "device_id" : 120,
-        "period" : 8070,
-        "freq" : 57,
-        "ant_name" : "C:HRM",
-        "handler" : "_handle_hr"
-      },
-      "power" : {
-        "device_id" : 11,
-        "period" : 8182,
-        "freq" : 57,
-        "ant_name" : "C:PWR",
-        "handler" : "_handle_power"
-      }
-    }
-
-    self.parsers = {
-      "hr" : antparsers.Hr(),
-      "power" : antparsers.Power(),
-      "speed_cad" : antparsers.SpeedCadence()
-    }
-    self._log_raw_data = False
-    self._log_decoded = True
-
-  def start(self):
-    if not os.path.isdir("logs"):
-      os.mkdir("logs")
-    self.logfile = datetime.strftime(datetime.now(), "logs/%Y%m%d_%H%M.txt")
-    self.logfile_handle = open(self.logfile, "w")
-
-    self._setup_channels()
-
-  def stop(self):
-    self.antnode.stop()
-
-  def __enter__(self):
-    return self
-
-  def _setup_channels(self):
-    self.antnode = Node()
-    self.antnode.set_network_key(0x00, self.netkey)
-
-    for device in self.ant_devices:
-      print("Registering device : %s : %s" % (device, self.ant_modes[device]))
-      dev = self.ant_modes[device]
-
-      c = self.antnode.new_channel(Channel.Type.BIDIRECTIONAL_RECEIVE)
-      c.set_id(0, dev["device_id"], 0)
-      c.set_search_timeout(12)
-      c.set_period(dev["period"])
-      c.set_rf_freq(dev["freq"])
-      m = getattr(self, dev["handler"])
-      c.on_broadcast_data = m
-      c.on_burst_data = m
-      c.open()
-      self.channels.append(c)
-
-    self.antnode.start()
-
-  def _log_raw(self, event_type, msg):
-    if self._log_raw_data:
-      data = []
-      for m in msg:
-        data.append(m)
-      m = json.dumps({"raw" : "true",
-                      "event_type" : event_type,
-                      "time_millis" : int(time.time() * 1000),
-                      "data" : data})
-      self.logfile_handle.write(m)
-      self.logfile_handle.write("\n")
-      self.logfile_handle.flush()
-      os.fsync(self.logfile_handle.fileno())
-
-  def _parse_and_send(self, event_type, data):
-    self._log_raw(event_type, data)
-
-    parser = self.parsers[event_type]
-    values = parser.parse(data)
-    if values:
-      for value in antparsers.Parser.to_json(values):
-        self.was.send_to_all(value)
-        if self._log_decoded:
-          print(value)
-
-  def _handle_hr(self, data):
-    self._parse_and_send("hr", data)
-
-  def _handle_speed_cad(self, data):
-    self._parse_and_send("speed_cad", data)
-
-  def _handle_power(self, data):
-    self._parse_and_send("power", data)
-
-  def __exit__(self, type_, value, traceback):
-    self.stop()
-    self.logfile_handle.close()
 
 
 class LogReplayer():
@@ -216,26 +96,17 @@ class Paincave():
     except KeyboardInterrupt:
       pass
 
-  def _run_antserver(self):
-    ant_server = AntServer(netkey=self._NETKEY, \
-                           ant_devices = ["hr", "speed_cad", "power"], \
-                           was = self._websocket_ant_server)
-    ant_server._log_decoded = self._args.v
-    ant_server._log_raw_data = self._args.enable_log
+  def _setup_and_start_antserver(self):
     try:
-      ant_server.start()
+      #todo
+      log_config = {"log_decoded" : self._args.v,
+                    "log_raw_data" : self._args.enable_log}
+      antserver = AntServer.setup_and_start(netkey=self._NETKEY, \
+                                ant_devices = ["hr", "speed_cad", "power"], \
+                                was = self._websocket_ant_server,
+                                log_config = log_config)
     except KeyboardInterrupt:
       pass
-    finally:
-      ant_server.stop()
-
-  def _setup_antserver(self):
-    for i in range(1, 3):
-      try:
-        self._run_antserver()
-        break
-      except AntException:
-        print("ERR: Failed to setup ant server. Retrying...", i)
 
   def main(self):
     self._parse_args()
@@ -247,7 +118,7 @@ class Paincave():
       if self._args.replay_log:
         self._replay_log()
       else:
-        self._setup_antserver()
+        self._setup_and_start_antserver()
     finally:
       print("INF: Killing websocket")
       self._websocket_ant_server.stop()
